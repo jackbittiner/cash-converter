dfa9ed11525f96747a275f2fca9de5df53b1a604
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-08 13:53:29 +0100

    :sparkles: add second dropdown

--- packages/client/src/components/cash-converter/cash-converter.js
+++ packages/client/src/components/cash-converter/cash-converter.js
@@ -4,14 +4,22 @@ import { Dropdown } from "semantic-ui-react";
 
 function CashConverter({ getConversion, convertedAmount }) {
   const [dropdownOneValue, setDropdownOneValue] = useState(null);
-
-  console.log(dropdownOneValue);
+  const [dropdownTwoValue, setDropdownTwoValue] = useState(null);
 
   return (
     <div>
       <Dropdown
-        onChange={(e, value) => {
-          setDropdownOneValue(value.value);
+        onChange={(e, selection) => {
+          setDropdownOneValue(selection.value);
+        }}
+        placeholder="Select Currency"
+        fluid
+        selection
+        options={currencyOptions}
+      />
+      <Dropdown
+        onChange={(e, selection) => {
+          setDropdownTwoValue(selection.value);
         }}
         placeholder="Select Currency"
         fluid
